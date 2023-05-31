ac99b25cf09058bfc149b9de1e78aa34b9a015cc
Mahesh <moholkarmahesh@aca.edu.in>
2023-05-31 11:46:32 +0530

    Added backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,28 @@
 import React from "react"
 import {StreamChat} from 'stream-chat'
 import {Chat} from 'stream-chat-react'
+import Cookies from "universal-cookie"
 
 import {ChannelListContainer, ChannelContainer, Auth} from './components'
 
 import './App.css'
 
-const apiKey = 'e6saq9tspkuw'
+const cookies = new Cookies()
 
+const apiKey = 'e6saq9tspkuw'
+const authToken = cookies.get('token')
 const client = StreamChat.getInstance(apiKey)
 
-const authToken = false
+if(authToken){
+  client.connectUser({
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    image: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+    phoneNumber: cookies.get('phoneNumber'),
+  }, authToken)
+}
 
 const App = () => {
   
